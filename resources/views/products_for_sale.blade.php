ad934488f8f3f3c1ac37efad72e2c179b4ef743f
Jose Jaime Come <josejaimecome@gmail.com>
2023-02-09 19:54:55 +0200

    Add item to chart functions

--- resources/views/products_for_sale.blade.php
+++ resources/views/products_for_sale.blade.php
@@ -10,11 +10,47 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.2.1/axios.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 
+    <style>
+        .addToChrt { background-color: #239B56 }
+        .rmvToChrt { background-color: #FF5733 }
+
+        .notification {
+            color: black;
+            text-decoration: none;
+            padding: 1px 12px;
+            position: relative;
+            display: inline-block;
+            border-radius: 2px;
+        }
+
+        .notification:hover {
+            background: red;
+        }
+
+        .notification .badge {
+           position: absolute;
+           top: -1px;
+           right: -1px;
+           padding: 5px 10px;
+           border-radius: 50%;
+           background-color: red;
+           color: white;
+        }
+        .Icn{
+           font-size: 2em;
+        }
+    </style>
 </head>
 <body>
 <div class="album py-5 bg-light">
     <div class="container">
         <div id="app" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div style="height: 60px; width: 100%;">
+            <a href="#" class="notification" style="float: right;">
+                <span class="bi bi-cart-check Icn"></span>
+                <span class="badge">[[ count ]]</span>
+            </a>
+        </div>
         <div v-for="product_item in list_of_products" :key="id" class="col">
             <div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: fffff text" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/>
@@ -25,10 +61,16 @@
               <p class="card-text">[[ product_item.description ]]</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                  <button type="button"  @click="test" class="btn btn-sm btn-outline-secondary" style='background-color: #FF5733; color: white;'>[[ product_status[product_item.id] !== null && product_status[product_item.id] === 1 ? 'Remove from Chart' : 'Add to Chart' ]]</button>
+                  <button type="button"  @click="checkProduct(product_item.id, product_item.product)"
+                        class="btn btn-sm btn-outline-secondary"
+                        style="color: white;"
+                        :class="[ product_status[product_item.id] === 1 ? 'addToChrt' : 'rmvToChrt' ]"
+                    >
+                        [[ product_status[product_item.id] !== null && product_status[product_item.id] === 1 ? 'Remove from Chart' : 'Add to Chart' ]]
+                   </button>
                   <!--<button type="button" class="btn btn-sm btn-outline-secondary">qq</button>-->
                 </div>
-                <div class="bi bi-cart-check" style="color: #239B56"></div>
+                <div v-if="product_status[product_item.id] === 1" class="bi bi-cart-check Icn" style="color: #239B56;"></div>
               </div>
             </div>
             </div>
@@ -44,7 +86,8 @@
     data: () => {
       return {
         list_of_products: [],
-        product_status: []
+        product_status: [],
+        count: 0
       }
     },
     async created() {
@@ -57,14 +100,11 @@
         }
     },
     methods: {
-        test(){
-            console.log("Print:" + id + "," + status)
+        checkProduct: function (id, b){
+            //alert("Print:" + a + "," + b);
+            this.product_status[id] = 1;
+            this.count++;
         },
-        checkProduct(id, status){
-            console.log("Print:" + id + "," + status)
-            this.product_status[id] = status;
-        }
-
     }
   }).mount('#app')
 </script>
