b0bd1f590f8f0705153979ec0ba5eb4b461f6280
Jose Jaime Come <josejaimecome@gmail.com>
2023-03-31 15:34:10 +0200

    Minor changes

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
+use App\Models\ProdCategories;
+use App\Models\Product;
+use App\http\Traits\saveImages;
+use Exception;
+
+class CategoryController extends Controller
+{
+    use saveImages;
+    function getCategories()
+    {
+        $data = ProdCategories::all();
+        return view('home',['prodcat'=>$data]);
+    }
+    function addNewCategory(Request $req)
+    {
+        $result = "Record Successfully added!";
+
+        if (! Gate::allows('addupdate_ctgry')) {
+            //abort(403);
+        }
+
+        $userId = 1;// Auth::id();
+        //$user = Auth::user();
+
+        $type_of_item = "prod_categories";
+        $userId = 1;//Auth::id();
+        $filename = $this->categories_products_image($req,  $type_of_item);
+        if($filename === "No file") {
+            $filename = $req->image_link;
+        }
+        if(!$req->filled('id')) {
+            try{
+                $ctgry = new ProdCategories;
+                $ctgry->category = $req->category;
+                $ctgry->description = $req->description;
+                $ctgry->image_link = $filename;
+                $ctgry->user_id = $userId;
+                $ctgry->save();
+            } catch(Exception $e) {
+                $result = 'Error ocurred: ' . $e->getMessage();
+            }
+        } else {
+            try{
+                $category_product = ProdCategories::updateOrCreate(
+                   ['id' => $req->id],
+                   ['category' => $req->category, 'description' => $req->description,
+                    'image_link' => $filename, 'user_id' => $userId]
+                );
+            $result = $category_product->wasChanged() ? "updated" : "not updated";
+        } catch(Exception $e) {
+            $result = 'Error ocurred: ' . $e->getMessage();
+        }
+        }
+        return Redirect::to('/add_successfull?p=' . $result);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,13 @@ use Illuminate\Http\Request;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/',[ProdCategoriesController::class,'getProdCategories']);
-Route::get('/product/{category}',[ProdCategoriesController::class, 'getProducts'])->name('product');
-Route::get('/product_form',[ProdCategoriesController::class, 'addNewProductForm'])->name('product_form');
-Route::post('/add_product',[ProdCategoriesController::class, 'addNewProduct']);
-Route::post('/add_category',[ProdCategoriesController::class, 'addNewCategory'])->name('add_category');
-Route::get('/products_for_sale/{category}',[ProdCategoriesController::class, 'ProductsForSale'])->name('products_for_sale');
-Route::get('/products_for_sale_list/{category}',[ProdCategoriesController::class, 'ProductsForSaleList'])->name('products_for_sale_list');
+Route::get('/',[CategoryController::class,'getCategories']);
+Route::get('/product/{category}',[ProductController::class, 'getProducts'])->name('product');
+Route::get('/product_form',[ProductController::class, 'addNewProductForm'])->name('product_form');
+Route::post('/add_product',[ProductController::class, 'addNewProduct']);
+Route::post('/add_category',[CategoryController::class, 'addNewCategory'])->name('add_category');
+Route::get('/products_for_sale/{category}',[ProductController::class, 'ProductsForSale'])->name('products_for_sale');
+Route::get('/products_for_sale_list/{category}',[ProductController::class, 'ProductsForSaleList'])->name('products_for_sale_list');
 Route::get('/add_successfull', function () {
     return view('add_successfull');
 });
