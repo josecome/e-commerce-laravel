f6c7be50ce2b2277d1ab5b0ee14ce78e7857de2e
Jose Jaime Come <josejaimecome@gmail.com>
2023-04-24 21:47:33 +0200

    Methods for Rest API added

---
+++ app/Http/Controllers/UserApiController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserApiController extends Controller
+{
+    public function login(Request $req)
+    {
+        if (!Auth::attempt($req->only('email', 'password'))) {
+            //return response()->json([ 'message' => 'Invalid login details', 401 ]);
+            return response()->json([ 'message' => $req["email"] . ',' . $req->password, 401 ]);
+        }
+
+        $user = User::where('email',  $req->email)->firstOrFail();
+
+        auth()->user()->tokens()->delete();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function register(Request $req)
+    {
+        $user = User::create([
+              "name" => $req->name,
+              "email" => $req->email,
+              "password" => Hash::make($req->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            "user"  => $user,
+            "token"  => $token
+        ]);
+    }
+
+    public function logout(Request $req) {
+        //Session::flush();
+        //Auth::logout();
+        //return redirect('login');
+        $req->user()->currentAccessToken()->delete();
+        return 'logged out';
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [UserApiController::class, 'register'])->name('register');
+Route::post('/login', [UserApiController::class, 'login'])->name('login');
+Route::post('/logout', [UserApiController::class, 'logout'])->name('logout');
